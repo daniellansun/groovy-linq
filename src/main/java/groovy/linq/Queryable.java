4602ee1936e6e12db851857332f4b6edff97fc68
Daniel Sun <sunlan@apache.org>
2019-08-11 00:39:11 +0800

    Format code

--- src/main/java/groovy/linq/Queryable.java
+++ src/main/java/groovy/linq/Queryable.java
@@ -30,33 +30,51 @@ import java.util.stream.Stream;
 
 public interface Queryable<T> {
     <U> Queryable<Tuple2<T, U>> innerJoin(Queryable<? extends U> queryable, BiPredicate<? super T, ? super U> joiner);
+
     <U> Queryable<Tuple2<T, U>> leftJoin(Queryable<? extends U> queryable, BiPredicate<? super T, ? super U> joiner);
+
     <U> Queryable<Tuple2<T, U>> rightJoin(Queryable<? extends U> queryable, BiPredicate<? super T, ? super U> joiner);
+
     default <U> Queryable<Tuple2<T, U>> fullJoin(Queryable<? extends U> queryable, BiPredicate<? super T, ? super U> joiner) {
         Queryable<Tuple2<T, U>> lj = this.leftJoin(queryable, joiner);
         Queryable<Tuple2<T, U>> rj = this.rightJoin(queryable, joiner);
         return lj.union(rj);
     }
+
     <U> Queryable<Tuple2<T, U>> crossJoin(Queryable<? extends U> queryable);
+
     Queryable<T> where(Predicate<? super T> filter);
+
     <K> Queryable<Tuple2<K, Queryable<T>>> groupBy(Function<? super T, ? extends K> classifier, BiPredicate<? super K, ? super Queryable<? extends T>> having);
+
     default <K> Queryable<Tuple2<K, Queryable<T>>> groupBy(Function<? super T, ? extends K> classifier) {
         return groupBy(classifier, (k, l) -> true);
     }
+
     <U extends Comparable<? super U>> Queryable<T> orderBy(Order<? super T, ? extends U>... orders);
+
     Queryable<T> limit(int offset, int size);
+
     default Queryable<T> limit(int size) {
         return limit(0, size);
     }
+
     <U> Queryable<U> select(Function<? super T, ? extends U> mapper);
+
     Queryable<T> distinct();
+
     default Queryable<T> union(Queryable<? extends T> queryable) {
         return this.unionAll(queryable).distinct();
     }
+
     Queryable<T> unionAll(Queryable<? extends T> queryable);
+
     Queryable<T> intersect(Queryable<? extends T> queryable);
+
     Queryable<T> minus(Queryable<? extends T> queryable);
+
     List<T> toList();
+
     default Stream<T> stream() {
         return toList().stream();
     }
